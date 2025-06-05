abb055653859e3aaa934a57cb3ceecb6a4476b89
Luiz Galiza <lfdgaliza@gmail.com>
2025-06-05 10:13:40 +0100

    Let's start again with a more moderate usage of AI

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace openfga_experiment.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
